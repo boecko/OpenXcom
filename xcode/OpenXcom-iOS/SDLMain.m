72c0203c03262cbbb5da0d89cad77ffbb7a70d10
Andreas Boeckler <andy@boeckler.org>
2014-09-17 13:46:17 +0200

    adjusted Info.plist (still had OS-X NSApplication in it)

--- xcode/OpenXcom-iOS/SDLMain.m
+++ xcode/OpenXcom-iOS/SDLMain.m
@@ -9,6 +9,39 @@
 #include "SDL.h"
 #include "SDLMain.h"
 
-@implementation SDLMain
+#ifdef IOS_SDLMAIN
+#ifdef main
+#  undef main
+#endif
 
+int main(int argc, char *argv[]) {
+    int retVal = 0;
+    @autoreleasepool {
+        @try {
+            retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([SDLMain class]));
+        }
+        @catch (NSException *exception) {
+            NSLog(@"Exception - %@",[exception description]);
+            NSLog(@"%@", [exception callStackSymbols]);
+            exit(EXIT_FAILURE);
+        }
+    }
+    return retVal;
+}
+
+@implementation SDLMain
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    int status;
+    
+    /* Set the working directory to the .app's parent directory */
+        //[self setupWorkingDirectory:gFinderLaunch];
+    
+    /* Hand off to main application code */
+    status = SDL_main (NULL, NULL);
+    
+    /* We're done, thank you for playing */
+    exit(status);
+}
 @end
+#endif
